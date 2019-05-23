fbf66e9c694c68f49ea1e1545c2644d3cdc3c877
henkwillem <henkwillemdeboer@outlook.com>
2019-05-23 12:25:42 +0200

    EIndopdracht

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -2,14 +2,51 @@
 // Op die mommenten komt er een class op de body.
 sounds = [
 	0,
-	9,
+	9, 
+    10,
 	12,
+    13,
 	15,
 	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+    17.1,
+    18, //sound8 cells1 aan
+    19.3, //9 - cells2 aan
+    20.8, //10 cells3 aan
+    21.7, //11 cells3 uit
+    22.0, //12 one stem aan
+    22.9, //13 one stem uit
+	31, //14
+	32, //15
+    33, //16 begin cells
+    33.4, //17
+	36, //18
+    36.4, //19
+    38, //20
+    38.4, //21
+    38.9, //22
+    42.9, //23
+    43.3, //24
+    44, //25
+    45.1, //interlinked 2
+    47.7, //27 interlinked 3
+    48.2, //28 interlinked 4
+    51, //29 interlinked 5
+    51.8, //30 interlinked 6
+    53.2, //31 interlinked 7
+    54, //32 interlinked 8
+    54.6, //33 interlinked 9
+    56.5, //34 interlinked 10
+    57.2, //35 interlinked 11
+    59.9, //36 interlinked 12
+    60.7, //37 interlinked 13
+    63, //38 interlinked 14
+    63.6, //39 interlinked 15
+    64, //40 within cells interlinked 1
+    65.7, //41 within cells interlinked 2
+    69.2, //42 within cells interlinked 1/3
+    70.8, //43 within cells interlinked 2/3
+    72.2, //44 within cells interlinked 3/3
+    74.2, //45 gekke gif
+    75, //46 rust
+    86, 
 ];
